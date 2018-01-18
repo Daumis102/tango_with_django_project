5dd01488f83f587cbf0fe47eb6f13aa950d94ced
Daumantas Pagojus <2267352p@student.gla.ac.uk>
2018-01-18 12:53:23 +0000

    fixes1

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     context_dict = {'boldmessage': "Chrunchy, creamy,cookie,candy,cupcake!"}
-    
+    print (render(request, 'rango/index.html', context=context_dict))
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
