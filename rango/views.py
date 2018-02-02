7473a1549bbb8f33161517f0245fd3f52b863a00
Daumantas Pagojus <2267352p@student.gla.ac.uk>
2018-02-02 09:55:27 +0000

    craeted login view

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm
-from tango.forms import UserForm, UserProfileForm
+from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login
+from django.core.urlresolvers import reverse
+
 
 def index(request):
     # Query the database for a list of all the categories currently storred
@@ -109,11 +112,11 @@ def register(request):
     registered = False
 
     # if its a HTTP POST we're interested in processing form data.
-    if request_method == 'POST'
+    if request.method == "POST":
         # atempt to frab information from the raw form information.
         # Note that we make use of both UserForm and UserProfileForm
         user_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(data=request.POST
+        profile_form = UserProfileForm(data=request.POST)
 
         # if the two forms are valud...
         if user_form.is_valid() and profile_form.is_valid():
@@ -162,7 +165,48 @@ def register(request):
                    'profile_form': profile_form,
                    'registered': registered})
         
+def user_login(request):
+    # if the request is a HTTP POST, try to pull out the relevant information
+    if request.method == 'POST':
+        # Gather the username and password provided by the user
+        # This information is obtained from the login form/
+        # We user request.POST.get('<variable>') as opposed
+        # to request.POST['<variable>'], because the
+        # request.POST.get('<variable>') returns None if the
+        # value does not exist, while other will raise a KeyError exception.
 
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Use Django's machinery to attempt to see if the username/password
+        # combination is valid - a User object is returned if it is
+        user = authenticate(username=username, password=password)
+
+        # if we have a User object, the details are correct.
+        # if None, no user with matching credentials was found
+
+        if user:
+            # Is the account active? It could have been dissabled.
+            if user.is_active:
+                # if the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                # an inactive account was used - no logging in!
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            # Bad login credentials were provided. So we can't log the user in.
+            print("invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+
+    # The request is not a HTTP POST, so display the login form.
+    # This scenario would most likely be HTTP GET
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object..
+        return render(request, 'rango/login.html',{})
+        
 
                                        
     
